48d59320e5060fad8971abaee76b2e36d2bd428c
mdekerga <mathieu.dekergariou@gmail.com>
2025-09-27 17:45:15 +0200

    front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FilmComponentComponent } from './film-component/film-component.component';
+import { ButtonComponent } from './button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FilmComponentComponent, ButtonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'kelfilm';
---
+++ src/app/film-component/film-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-component',
+  imports: [],
+  templateUrl: './film-component.component.html',
+  styleUrl: './film-component.component.css'
+})
+export class FilmComponentComponent {
+
+}
